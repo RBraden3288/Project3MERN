631de90423fc5f4ae50b325b8cf93101c2c6a4db
Randall <46095063+RBraden3288@users.noreply.github.com>
2019-07-31 15:43:00 -0700

    Delete index.js

--- client/src/components/Navbar/index.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// and if we plan on customizing CSS, we'll have to import the css file within this folder, too
-
-// function Navbar() {
-// return (
-// reactstrap elements ft. links to our GitHub repo? If we rather put it here than in the footer
-// )
-// }
