06d4990d822b0ff90116555ed06d3653090298d8
racheldmiller <rachelmiller415@gmail.com>
2019-08-05 21:04:37 -0700

    commented out the controller file.. will get back to it later

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,32 +7,32 @@ const db = require("../models");
 // ---------------- CREATING ALL OF THE DB QUERIES  ----------------
 // copied and pasted from booksapp -- needs to be customized for our app
 
-module.exports = {
-  findAll: function(req, res) {
-    db.Requests.find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  findById: function(req, res) {
-    db.Requests.findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
-    db.Requests.create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  update: function(req, res) {
-    db.Requests.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  remove: function(req, res) {
-    db.Requests.findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
-};
+// module.exports = {
+// //   findAll: function(req, res) {
+// //     db.Requests.find(req.query)
+// //       .sort({ date: -1 })
+// //       .then(dbModel => res.json(dbModel))
+// //       .catch(err => res.status(422).json(err));
+// //   },
+// //   findById: function(req, res) {
+// //     db.Requests.findById(req.params.id)
+// //       .then(dbModel => res.json(dbModel))
+// //       .catch(err => res.status(422).json(err));
+// //   },
+// //   create: function(req, res) {
+// //     db.Requests.create(req.body)
+// //       .then(dbModel => res.json(dbModel))
+// //       .catch(err => res.status(422).json(err));
+// //   },
+// //   update: function(req, res) {
+// //     db.Requests.findOneAndUpdate({ _id: req.params.id }, req.body)
+// //       .then(dbModel => res.json(dbModel))
+// //       .catch(err => res.status(422).json(err));
+// //   },
+// //   remove: function(req, res) {
+// //     db.Requests.findById({ _id: req.params.id })
+// //       .then(dbModel => dbModel.remove())
+// //       .then(dbModel => res.json(dbModel))
+// //       .catch(err => res.status(422).json(err));
+// //   }
+// };
